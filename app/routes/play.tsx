3294e2176c6ccc95124cf9dcb3b31602b476ad83
leomiranda <leonardomir@gmail.com>
2024-09-28 12:31:52 -0400

    refactor: search

--- app/routes/play.tsx
+++ app/routes/play.tsx
@@ -175,12 +175,33 @@ export default function MainPage() {
 					)}
 				</nav>
 				<div className="ml-auto flex items-center gap-4">
-					<form className="relative hidden sm:block">
-						<Search className="absolute left-2 top-1/2 transform -translate-y-1/2 text-gray-400" />
+					<form
+						className="min-w-10 relative hidden sm:block"
+						onBlur={(e) => {
+							if (!e.currentTarget.contains(e.relatedTarget)) {
+								const input = document.getElementById('search-input');
+								if (input) {
+									input.classList.add('hidden');
+								}
+							}
+						}}
+					>
+						<Search
+							className="absolute left-2 top-1/2 transform -translate-y-1/2 text-white cursor-pointer hover:text-gray-400"
+							onClick={() => {
+								const input = document.getElementById('search-input');
+								if (input) {
+									input.classList.remove('hidden');
+									input.classList.add('animate-fade-in');
+									input.focus();
+								}
+							}}
+						/>
 						<Input
+							id="search-input"
 							type="search"
 							placeholder="Search classes..."
-							className="pl-8 bg-gray-800 text-white placeholder-gray-400 border-gray-700"
+							className="pl-10 bg-gray-800 text-white placeholder-gray-400 border-gray-700 hidden animate-fade-out"
 						/>
 					</form>
 					<Button variant="ghost" size="icon" className="hidden sm:inline-flex">
